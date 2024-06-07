fb85bfc3e9d0b9aba4eb5b067a73ea402d99470e
gEloig <eebmp.geg@educacao.arcos.mg.gov.br>
2024-06-06 21:57:45 -0300

    Tabuada
    06/06/2024

---
+++ ExibirTabuada.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExibirTabuada
+{
+    public static void main(String[] args) {
+        int t;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o numero de que você deseje ver a tabuada:");
+        int n = scanner.nextInt();
+        
+        for (int i = 0; i <= 10 ; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+        scanner.close();
+    }
+}
